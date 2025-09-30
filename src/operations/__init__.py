118cf216788f6acc23734c8c4b2c5ff3af91b134
samgonzalez27 <seg42@njit.edu>
2025-09-30 15:46:11 -0400

    Fix: Improved src by after linting

--- src/operations/__init__.py
+++ src/operations/__init__.py
@@ -1,20 +1,19 @@
 """Basic arithmetic operations of add, subtract, multiply, divide"""
 
 def add(a, b):
-	"""Return the sum of a and b."""
-	return a + b
+    """Return the sum of a and b."""
+    return a + b
 
 def subtract(a, b):
-		"""Return the difference of a and b."""
-		return a - b
+    """Return the difference of a and b."""
+    return a - b
 
 def multiply(a, b):
-	"""Return the product of a and b."""
-	return a * b
+    """Return the product of a and b."""
+    return a * b
 
 def divide(a, b):
-	"""Return the result of dividing a by b. Raises ZeroDivisionError if b == 0."""
-	if b == 0:
-		raise ZeroDivisionError("division by zero")
-	return a / b
-
+    """Return the result of dividing a by b. Raises ZeroDivisionError if b == 0."""
+    if b == 0:
+        raise ZeroDivisionError("division by zero")
+    return a / b
