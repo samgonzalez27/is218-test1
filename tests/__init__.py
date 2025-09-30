41b6c1abbb5aff4163a7c9c6d97371e64f735cb4
samgonzalez27 <seg42@njit.edu>
2025-09-30 15:33:51 -0400

    Feat: Implement multiply() with unit test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 
-from src.operations import add, subtract
+from src.operations import add, subtract, multiply
 
 def test_add():
 	assert add(2, 3) == 5
@@ -7,3 +7,7 @@ def test_add():
 
 def test_subtract():
 	assert subtract(5, 3) == 2
+
+
+def test_multiply():
+	assert multiply(4, 3) == 12
